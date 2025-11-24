4cbb5df4ff97af293329cf9274b63d56e1f08bf5
Oscar <oscarodriguez56@gmail.com>
2025-11-24 10:26:01 +0100

    Añadiendo modelos

---
+++ src/main/java/org/example/model/Alumno.java
@@ -0,0 +1,62 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alumno")
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_alumno")
+    private Long id;
+
+    @Column(length = 100,nullable = false)
+    private String nome;
+
+    @Column(length = 150,nullable = false)
+    private String apelidos;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "titor",referencedColumnName = "id_titor")
+    private Titor titor;
+
+    public Alumno(Long id, String nome, String apelidos, Titor titor) {
+        this.id = id;
+        this.nome = nome;
+        this.apelidos = apelidos;
+        this.titor = titor;
+    }
+    public Alumno() {}
+
+    public Titor getTitor() {
+        return titor;
+    }
+
+    public void setTitor(Titor titor) {
+        this.titor = titor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getApelidos() {
+        return apelidos;
+    }
+
+    public void setApelidos(String apelidos) {
+        this.apelidos = apelidos;
+    }
+}
---
+++ src/main/java/org/example/model/Titor.java
@@ -0,0 +1,50 @@
+package org.example.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "titor")
+public class Titor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_titor")
+    private Long id;
+
+    @Column(length = 100,nullable = false)
+    private String nome;
+
+    @Column(length = 150,nullable = false)
+    private String apelidos;
+
+    public Titor(Long id, String nome, String apelidos) {
+        this.id = id;
+        this.nome = nome;
+        this.apelidos = apelidos;
+    }
+    public Titor() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelidos() {
+        return apelidos;
+    }
+
+    public void setApelidos(String apelidos) {
+        this.apelidos = apelidos;
+    }
+}
