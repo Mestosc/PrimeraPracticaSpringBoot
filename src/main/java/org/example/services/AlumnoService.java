156ec02405e51194cb2597201ad5eea67e7c0a48
Oscar <oscarodriguez56@gmail.com>
2025-12-01 11:45:11 +0100

    Remover: Método `obtenerAlumnosTitor` en `AlumnoService`.

--- src/main/java/org/example/services/AlumnoService.java
+++ src/main/java/org/example/services/AlumnoService.java
@@ -36,7 +36,4 @@ public class AlumnoService {
             return a;
         });
     }
-    public Optional<List<Alumno>> obtenerAlumnosTitor(Titor titor) {
-        return alumnoRepository.findByTitor(titor);
-    }
 }
