bfb19f3aba78e72b4eec320c8a45421c6c3a4995
Oscar <oscarodriguez56@gmail.com>
2025-12-01 09:22:51 +0100

    Refactor: Eliminar imports y métodos no utilizados en AlumnoRepository.

--- src/main/java/org/example/repository/AlumnoRepository.java
+++ src/main/java/org/example/repository/AlumnoRepository.java
@@ -1,12 +1,8 @@
 package org.example.repository;
 
 import org.example.model.Alumno;
-import org.example.model.Titor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AlumnoRepository extends JpaRepository<Alumno,Long> { }
