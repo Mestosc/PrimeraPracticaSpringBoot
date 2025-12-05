ce5ed4c0a00b491d7c0f0e8c6408c83580df9ea7
Oscar <oscarodriguez56@gmail.com>
2025-12-05 10:24:03 +0100

    Eliminar línea en blanco del controlador de alumnos.

--- src/main/java/org/example/controller/AlumnosController.java
+++ src/main/java/org/example/controller/AlumnosController.java
@@ -5,7 +5,6 @@ import org.example.services.AlumnoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.Optional;
 
 @RestController
